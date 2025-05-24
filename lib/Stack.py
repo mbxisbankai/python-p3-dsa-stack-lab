ceea32f71ded6b39c778e2c7717b8748ce257285
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-05-24 11:58:54 +0300

    DSA Stack Lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,33 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            return self.items.append(item)
+        return None
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        return None
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            return self.size() - 1 - self.items.index(target)
+        except ValueError:
+            return -1
